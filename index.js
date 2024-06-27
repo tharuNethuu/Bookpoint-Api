537e6475201a37722b030bf98d8b599f057a334e
tharuNethuu <150820780+tharuNethuu@users.noreply.github.com>
2024-06-27 23:08:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://bookpoint-client.vercel.app'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
